89f895bfcd7d79eac057b7f8a20fbef96e929bf5
Kamil Kijak <kamil.kijak@pixlesofte.com>
2025-11-05 23:00:19 +0100

    starting creating models

---
+++ models.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    surname = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
