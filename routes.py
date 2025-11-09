14d500321201fc1f67fe8a588f3286c210b018ba
Kamil Kijak <kamil.kijak@pixlesofte.com>
2025-11-09 22:19:27 +0100

    created working register, login to user account

--- routes.py
+++ routes.py
@@ -14,7 +14,7 @@ from database.models import db
 def main_page():
     if not session.get("idUser"):
         return redirect(url_for("welcome_page"))
-    return render_template("base.html")
+    return render_template("main.html")
 
 
 @app.route("/welcome")
@@ -29,7 +29,7 @@ def register_user():
             userSchema.load(data)
             # success
             if(db.session.query(User).filter(User.email == data.get("email")).count() > 0):
-                return render_template("register.html", errors={"email":["This email is already taken"]}, values=data)
+                return render_template("forms/register.html", errors={"email":["This email is already taken"]}, values=data)
             passwordBytes = str(data.get("password")).encode("utf-8")
             salt = bcrypt.gensalt()
             hash = bcrypt.hashpw(passwordBytes, salt)
@@ -44,10 +44,31 @@ def register_user():
             session["idUser"] = newUser.id
             return redirect(url_for("main_page"))
         except ValidationError as err:
-            return render_template("register.html", errors=err.messages, values=data)
+            return render_template("forms/register.html", errors=err.messages, values=data)
 
     else:
-        return render_template("register.html", values={})
+        return render_template("forms/register.html", values={})
+
+@app.route("/login", methods=["GET", "POST"])
+def login_user():
+    if request.method == "POST":
+        data = request.form.to_dict()
+        if db.session.query(User).filter(User.email == data.get("email")).count() == 0:
+            return render_template("forms/login.html", errors={"email":["User with this email doesn't exist"]}, values=data)
+        user = db.session.query(User).filter(User.email == data.get("email")).first()
+        if bcrypt.checkpw(str(data.get("password")).encode(), str(user.password).encode()):
+            # login successfully
+            session["idUser"] = user.id
+            return redirect(url_for("main_page"))
+        else:
+            return render_template("forms/login.html", errors={"password":["Invalid password"]}, values=data)
+    else:
+        return render_template("forms/login.html", values={})
+    
+@app.route("/logout")
+def logout_user():
+    session.clear()
+    return redirect(url_for("main_page"))
 
 @app.route('/assets/<path:filename>')
 def uploaded_file(filename):
