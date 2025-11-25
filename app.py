abd79bd91cf370b949db3ea6b9fe83db49c1891c
Kamil Kijak <kamil.kijak@pixlesofte.com>
2025-11-25 08:53:30 +0100

    fixes small bug

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ load_dotenv()
 # database
 from database.models import db
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = f'mysql://{os.getenv("DB_USER") or "root"}:{os.getenv("DB_PASSWORD") or ""}@{os.getenv("DB_HOST") or "localhost"}:{os.getenv("DB_PORT") or "root"}/{os.getenv("DB_NAME") or "evoting_flask_app_database"}'
+app.config["SQLALCHEMY_DATABASE_URI"] = f'mysql://{os.getenv("DB_USER") or "root"}:{os.getenv("DB_PASSWORD") or ""}@{os.getenv("DB_HOST") or "localhost"}:{os.getenv("DB_PORT") or "3306"}/{os.getenv("DB_NAME") or "evoting_flask_app_database"}'
 # app.config["SECRET_KEY"] = secrets.token_hex(32)
 app.config["SECRET_KEY"] = "7b62e655f1c8db498ea1fe56ee1deed2cee0e7ca9a6da2a79f7da0aab0379a1a"
 db.init_app(app)
